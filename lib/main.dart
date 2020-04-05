e6bccb03b2987de79e8c0127ac00d80eee2e5b78
yashodgayashan <yashodgayashan@gmail.com>
2020-04-05 20:50:04 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Tutorial",
+  home: Home(),
+));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("Hi"),);
+  }
+}
