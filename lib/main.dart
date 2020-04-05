af53fc82fbfc72d00ad882e0d8ae7a4fd494cf2a
yashodgayashan <yashodgayashan@gmail.com>
2020-04-06 05:01:29 +0530

    Added dropdownItems

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  title: "Tutorial",
-  home: Home(),
-));
+      title: "Tutorial",
+      home: Home(),
+    ));
 
 class Home extends StatefulWidget {
   @override
@@ -11,15 +11,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _value = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dropdown Button"),
-      ),
-      body: Container(
-        child: DropdownButton(items: null, onChanged: null),
-      )
-    );
+        appBar: AppBar(
+          title: Text("Dropdown Button"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          child: DropdownButton(
+              value: _value,
+              items: [
+                DropdownMenuItem(
+                  child: Text("First Item"),
+                  value: 1,
+                ),
+                DropdownMenuItem(
+                  child: Text("Second Item"),
+                  value: 2,
+                ),
+                DropdownMenuItem(
+                  child: Text("Third Item"),
+                  value: 3
+                ),
+                DropdownMenuItem(
+                    child: Text("Fourth Item"),
+                    value: 4
+                )
+              ],
+              onChanged: (value) {
+                setState(() {
+                  _value = value;
+                });
+              }),
+        ));
   }
 }
+
+
