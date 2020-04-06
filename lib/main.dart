66488f291f009f5b9d3c40162793485bcad4e36f
yashodgayashan <yashodgayashan@gmail.com>
2020-04-06 07:07:28 +0530

    update master

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      title: "Tutorial",
-      home: Home(),
-    ));
+  title: "Tutorial",
+  home: Home(),
+));
 
 class Home extends StatefulWidget {
   @override
@@ -11,17 +11,61 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<ListItem> _dropdownItems = [
+    ListItem(1, "First Value"),
+    ListItem(2, "Second Item"),
+    ListItem(3, "Third Item"),
+    ListItem(4, "Fourth Item")
+  ];
+
+  List<DropdownMenuItem<ListItem>> _dropdownMenuItems;
+  ListItem _selectedItem;
+
+  void initState() {
+    super.initState();
+    _dropdownMenuItems = buildDropDownMenuItems(_dropdownItems);
+    _selectedItem = _dropdownMenuItems[0].value;
+
+  }
+
+  List<DropdownMenuItem<ListItem>> buildDropDownMenuItems(List listItems) {
+    List<DropdownMenuItem<ListItem>> items = List();
+    for (ListItem listItem in listItems) {
+      items.add(
+        DropdownMenuItem(
+          child: Text(listItem.name),
+          value: listItem,
+        ),
+      );
+    }
+    return items;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter tutorial"),
+        title: Text("Dropdown Button"),
       ),
-      body: Center(
-          child: Text(
-        "Lets code",
-        style: TextStyle(fontSize: 20.0),
-      )),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: DropdownButton<ListItem>(
+            value: _selectedItem,
+            items: _dropdownMenuItems,
+            onChanged: (value) {
+              setState(() {
+                _selectedItem = value;
+              });
+            }),
+      ),
     );
   }
+}
+
+class ListItem {
+  int value;
+  String name;
+
+  ListItem(this.value, this.name);
 }
