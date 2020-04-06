487283c9f8d04df8f8eb1499aecb11d1deb72fcc
yashodgayashan <yashodgayashan@gmail.com>
2020-04-06 05:38:22 +0530

    Added listitem class

--- lib/main.dart
+++ lib/main.dart
@@ -11,44 +11,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   int _value = 1;
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Dropdown Button"),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(20.0),
-          child: DropdownButton(
-              value: _value,
-              items: [
-                DropdownMenuItem(
-                  child: Text("First Item"),
-                  value: 1,
-                ),
-                DropdownMenuItem(
-                  child: Text("Second Item"),
-                  value: 2,
-                ),
-                DropdownMenuItem(
-                  child: Text("Third Item"),
-                  value: 3
-                ),
-                DropdownMenuItem(
-                    child: Text("Fourth Item"),
-                    value: 4
-                )
-              ],
-              onChanged: (value) {
-                setState(() {
-                  _value = value;
-                });
-              }),
-        ));
+      appBar: AppBar(
+        title: Text("Dropdown Button"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: DropdownButton(
+            value: _value,
+            items: [
+              DropdownMenuItem(
+                child: Text("First Item"),
+                value: 1,
+              ),
+              DropdownMenuItem(
+                child: Text("Second Item"),
+                value: 2,
+              ),
+              DropdownMenuItem(
+                child: Text("Third Item"),
+                value: 3,
+              ),
+              DropdownMenuItem(
+                child: Text("Fourth Item"),
+                value: 4,
+              )
+            ],
+            onChanged: (value) {
+              setState(() {
+                _value = value;
+              });
+            }),
+      ),
+    );
   }
 }
 
+class ListItem{
+  int value;
+  String name;
 
+  ListItem(this.value, this.name);
+}
