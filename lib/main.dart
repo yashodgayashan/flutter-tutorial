1c18a1a58219feb9cf811aeaddfd522610bd2b74
yashodgayashan <yashodgayashan@gmail.com>
2020-04-07 10:05:38 +0530

    Added text Alignement

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,17 @@ void main() => runApp(MaterialApp(
       home: Home(),
     ));
 
-
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Text tutorial"),
-        ),
-        body:Text("Hi")
+      appBar: AppBar(
+        title: Text("Text tutorial"),
+      ),
+      body: Text(
+        "Hi",
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
