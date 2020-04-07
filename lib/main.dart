ec703b57954fccb9db689279f7d95e5b1d49b4b9
yashodgayashan <yashodgayashan@gmail.com>
2020-04-07 12:51:18 +0530

    Refactor the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/screens/text_tutorial.dart';
-import 'package:flutter_tutorial/screens/dropdown_screen.dart';
 
 void main() => runApp(MaterialApp(
       title: "Tutorial",
-      home: TextAlignScreen(),
+      home: Home(),
     ));
 
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Text tutorial"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            "This is a basic Text",
+          ),
+          Container(
+            width: width,
+            child: Text(
+              "Text with alignment and scale",
+              textAlign: TextAlign.right,
+              textScaleFactor: 2.0,
+            ),
+          ),
+          Text(
+            "text with styling",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.w700,
+              fontSize: 40.0,
+              letterSpacing: 2.0,
+              wordSpacing: 20.0,
+              fontFamily: "Caveat",
+            ),
+          ),
+          Text(
+            "text with styling",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.w400,
+              fontSize: 40.0,
+              letterSpacing: 2.0,
+              wordSpacing: 20.0,
+              fontFamily: "Caveat",
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  "$_value",
+                  textScaleFactor: 3.0,
+                ),
+              ),
+              Expanded(
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _value += 1;
+                    });
+                  },
+                  child: Text("Click"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
