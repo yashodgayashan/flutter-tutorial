8ab31f1bab52f4cc0d10eadbdac27ca8c076d8c5
yashodgayashan <yashodgayashan@gmail.com>
2020-04-07 12:46:43 +0530

    Complete the text tutorial

--- lib/screens/text_tutorial.dart
+++ lib/screens/text_tutorial.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-class TextAlignScreen extends StatelessWidget {
+class TextAlignScreen extends StatefulWidget {
+  @override
+  _TextAlignScreenState createState() => _TextAlignScreenState();
+}
+
+class _TextAlignScreenState extends State<TextAlignScreen> {
+  int _value = 0;
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -9,30 +16,64 @@ class TextAlignScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Text tutorial"),
       ),
-      body: Container(
-        width: width,
-        child: Column(
-          children: <Widget>[
-            Text(
-              "One",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.blue,
-                  fontFamily: "Caveat",
-                  fontWeight: FontWeight.w700),
+      body: Column(
+        children: <Widget>[
+          Text(
+            "This is a basic Text",
+          ),
+          Container(
+            width: width,
+            child: Text(
+              "Text with alignment and scale",
+              textAlign: TextAlign.right,
+              textScaleFactor: 2.0,
             ),
-            Text(
-              "Two",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.blue,
-                  fontFamily: "Caveat",
-                  fontWeight: FontWeight.w700),
+          ),
+          Text(
+            "text with styling",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.w700,
+              fontSize: 40.0,
+              letterSpacing: 2.0,
+              wordSpacing: 20.0,
+                fontFamily: "Caveat",
             ),
-          ],
-        ),
+          ),
+          Text(
+            "text with styling",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.w400,
+              fontSize: 40.0,
+              letterSpacing: 2.0,
+              wordSpacing: 20.0,
+              fontFamily: "Caveat",
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  "$_value",
+                  textScaleFactor: 3.0,
+                ),
+              ),
+              Expanded(
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _value += 1;
+                    });
+                  },
+                  child: Text("Click"),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
