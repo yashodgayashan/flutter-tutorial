1401e146f6a81a2df4b2b26f77a390a60f853bf7
yashodgayashan <yashodgayashan@gmail.com>
2020-04-08 16:08:36 +0530

    Completed expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/screens/text_tutorial.dart';
-import 'package:flutter_tutorial/screens/dropdown_screen.dart';
-import 'package:flutter_tutorial/screens/expanded_screen.dart';
 
-void main() => runApp(MaterialApp(
-      title: "Tutorial",
-      home: ExpandedScreen(),
-    ));
-
+void main() => runApp(
+      MaterialApp(
+        title: "Tutorial",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Expanded widget"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Text("One"),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Text("Two"),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Text("Three"),
+                  ),
+                ],
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.grey[300],
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          "One",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Two",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.grey[600],
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          "Ratio is 3",
+                          textAlign: TextAlign.center,
+                        ),
+                        flex: 3,
+                      ),
+                      Expanded(
+                          child: Text(
+                            "Ratio is 2",
+                            textAlign: TextAlign.center,
+                          ),
+                          flex: 2),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.grey[200],
+                  child: Row(
+                    children: <Widget>[
+                      Text("One"),
+                      Text("Two"),
+                    ],
+                  ),
+                ),
+              ),
+              Text("This is not a expanded widget"),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: Text("Hi"),
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
