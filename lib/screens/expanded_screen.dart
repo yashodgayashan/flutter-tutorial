a34802b4bb454db03ae67f1c5085ce363d488a5e
yashodgayashan <yashodgayashan@gmail.com>
2020-04-08 10:54:29 +0530

    Add expanded widget inside column

--- lib/screens/expanded_screen.dart
+++ lib/screens/expanded_screen.dart
@@ -3,8 +3,60 @@ import 'package:flutter/material.dart';
 class ExpandedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hi"),
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expanded widget"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey[300],
+              child: Row(
+                children: <Widget>[
+                  Text("One"),
+                  Text("Two"),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey[600],
+              child: Row(
+                children: <Widget>[
+                  Text("One"),
+                  Text("Two"),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey[200],
+              child: Row(
+                children: <Widget>[
+                  Text("One"),
+                  Text("Two"),
+                ],
+              ),
+            ),
+          ),
+          Text("This is not a expanded widget"),
+          Expanded(
+            flex: 3,
+            child: Container(
+              width: width,
+              child: Text("Hi"),
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
