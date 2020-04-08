40610faa77ba588661a094a2b01cd9c9568c8e64
yashodgayashan <yashodgayashan@gmail.com>
2020-04-08 08:02:23 +0530

    Integrate expanded widget with main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/screens/text_tutorial.dart';
 import 'package:flutter_tutorial/screens/dropdown_screen.dart';
+import 'package:flutter_tutorial/screens/expanded_screen.dart';
 
 void main() => runApp(MaterialApp(
       title: "Tutorial",
-      home: TextAlignScreen(),
+      home: ExpandedScreen(),
     ));
 
