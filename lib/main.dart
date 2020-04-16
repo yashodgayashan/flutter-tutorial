cff09ce19d9aee05a1828c53ee9b41b188cefe94
yashodgayashan <yashodgayashan@gmail.com>
2020-04-16 18:41:22 +0530

    Added validated form

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,116 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/screens/text_tutorial.dart';
-import 'package:flutter_tutorial/screens/dropdown_screen.dart';
-import 'package:flutter_tutorial/screens/expanded_screen.dart';
-import 'package:flutter_tutorial/screens/form_Screen.dart';
 
 void main() => runApp(MaterialApp(
       title: "Tutorial",
       home: FormScreen(),
     ));
 
+
+class FormScreen extends StatefulWidget {
+  @override
+  _FormScreenState createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Basic form"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(10.0),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter userName';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  labelText: 'UserName',
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(
+                    Icons.delete_outline,
+                    color: Colors.red,
+                  ),
+                  hintText: "YashodPerera",
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide:
+                    BorderSide(color: Colors.deepOrangeAccent, width: 2.0),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter age';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Age',
+                  hintText: "20",
+                  prefixIcon: Icon(Icons.date_range),
+                  suffixIcon: Icon(
+                    Icons.delete_outline,
+                    color: Colors.red,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide:
+                    BorderSide(color: Colors.deepOrangeAccent, width: 2.0),
+                  ),
+                ),
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  debugPrint("Valid");
+                }
+              },
+              child: Text("Submit"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
